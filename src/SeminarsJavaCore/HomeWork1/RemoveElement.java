970e35d228ac5f9d5f19c229ec81dd23cedb5606
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2022-08-05 16:00:01 +0600

    some commits

--- src/SeminarsJavaCore/HomeWork1/RemoveElement.java
+++ src/SeminarsJavaCore/HomeWork1/RemoveElement.java
@@ -2,16 +2,21 @@ package SeminarsJavaCore.HomeWork1;
 
 public class RemoveElement {
 
+    public static void main(String[] args) {
+
+    }
+
     public int removeElement(int[] nums, int val) {
-        int counter = 0;
+        int counter = 0; // counter of elements that have not been removed from the original array (nums)
 
+        // cycling all over the elements of nums:
         for (int i = 0; i < nums.length; i ++) {
-
+            // if the element is not removed:
             if (nums[i] != val) {
+                // if there have been no elements removed -> we do nothing here
                 if (counter != i) nums[counter] = nums[i];
-                counter++;
+                counter++; // incrementation of counter
             }
-
         }
 
         return counter;
--- src/SeminarsJavaCore/HomeWork1/ReverseWordsInString.java
+++ src/SeminarsJavaCore/HomeWork1/ReverseWordsInString.java
@@ -8,16 +8,17 @@ public class ReverseWordsInString {
 
     public static String reverseWords(String s) {
 
-        s = s.trim();
+        s = s.trim(); // removing the beginning and the ending spaces of String s
 
-        String[] words = s.split("\\s+");
-        reverseWordsArray(words);
+        String[] words = s.split("\\s+"); // splits by multiple spaces
+        reverseWordsArray(words); // reverses the array of words
 
-        return String.join(" ", words);
+        return String.join(" ", words); // builds the result String from the array of words and returns it
     }
 
+    // auxiliary method of array reversing
     public static void reverseWordsArray(String[] ws) {
-
+        // cycling until we reach the middle point
         for (int i = 0; i <= (ws.length - 1) / 2; i++) {
             String tempChar = ws[i];
             ws[i] = ws[ws.length - 1 - i];
